16cc03713da0e9a17fbcedb443fdb55d31eda677
kkoran10 <kwame.koranteng@hotmail.com>
2025-01-09 15:01:47 +0000

    added an API validation

--- cypress/e2e/categoryNavigation.cy.ts
+++ cypress/e2e/categoryNavigation.cy.ts
@@ -13,11 +13,19 @@ describe('template spec', () => {
 
     it ('user clicking on each of the CATEGORIES', () => {
         // logging in with valid credentials
+
+        cy.intercept('POST', 'https://api.demoblaze.com/bycat').as('filterByCategory')
+
+
         cy.visit('/')
 
         // UI validation that user has clicked on the 'Phones' category (ALTHOUGH NOT SURE HOW TO VALIDATE THIS AS URL DOES NOT CHANGE)
         cy.get('.list-group-item').contains('Phones').click();
 
+        cy.wait('@filterByCategory').then((object) => {
+            expect(object.response.statusCode).to.eq(200);
+
+        })
         // UI validation that user has clicked on the 'Laptops' category (ALTHOUGH NOT SURE HOW TO VALIDATE THIS AS URL DOES NOT CHANGE)
         cy.get('.list-group-item').contains('Laptops').click();
 
